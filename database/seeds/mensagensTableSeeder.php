0f8cf01e44d4a8db75b8e860a600dc618ee9e984
elen15ravazio <elen15ravazio@gmail.com>
2018-08-01 12:01:08 -0300

    mensagens table

---
+++ database/seeds/mensagensTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\mensagens;
+
+class mensagensTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
